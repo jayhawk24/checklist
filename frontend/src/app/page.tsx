583dcdb9c9b473e6ff8de3b10bded614f4fc087b
Anubhav Gupta <anubhavthrilling24@gmail.com>
2024-05-10 16:26:44 +0530

    feat: add home carousel

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,47 @@
-
+import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home</h1>
+      <div className="flex">
+
+        <Carousel
+          opts={{
+            align: "start", loop: true
+          }}
+          className="h-100 w-100"
+        >
+          <CarouselContent
+          >
+            <CarouselItem>
+              <Image src="/images/task5.svg" alt="Tasks" width={800} height={800} />
+            </CarouselItem>
+            <CarouselItem>
+              <Image src="/images/task3.svg" alt="Tasks" width={800} height={800} />
+            </CarouselItem>
+            <CarouselItem>
+              <Image src="/images/task4.svg" alt="Tasks" width={800} height={800} />
+            </CarouselItem>
+            <CarouselItem>
+              <Image src="/images/task6.svg" alt="Tasks" width={800} height={800} />
+            </CarouselItem><CarouselItem>
+              <Image src="/images/task7.svg" alt="Tasks" width={800} height={800} />
+            </CarouselItem>
+            <CarouselItem>
+              <Image src="/images/task2.svg" alt="Tasks" height={500} width={500} />
+            </CarouselItem>
+          </CarouselContent>
+        </Carousel>
+
+      </div>
     </main>
   );
 }
