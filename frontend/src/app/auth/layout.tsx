ae734a8a6ab1c4b7898cd51e15a2e60ce2efa62a
Anubhav Gupta <anubhavthrilling24@gmail.com>
2024-05-20 23:54:16 +0530

    fix: auth layout

--- frontend/src/app/auth/layout.tsx
+++ frontend/src/app/auth/layout.tsx
@@ -7,7 +7,7 @@ type Props = {
 const LayoutPage = ({ children }: Props) => {
     return (
         <main className="flex h-full flex-col md:mt-0 mt-6 md:p-24 p-5">
-            <div className="border border-secondary p-6 md:p-16 rounded-xl">
+            <div className="border border-secondary p-6 md:p-16 rounded-xl md:w-2/3">
                 {children}
             </div>
         </main>
