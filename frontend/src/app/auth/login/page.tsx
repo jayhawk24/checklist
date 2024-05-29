8cb576e5d9cdd3a6c8033dc947a2246ae717d131
Anubhav Gupta <anubhavthrilling24@gmail.com>
2024-05-29 22:24:47 +0530

    feat: add message

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -2,9 +2,12 @@ import Login from "@/components/login";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+
 export default function LoginPage() {
+
     return (
         <>
+
             <h1 className="text-5xl mb-20 text-center">Login</h1>
             <Login />
             <Link href={'/auth/signup'} className="w-full">
--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -4,10 +4,19 @@ import toast from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
 import { useUserProfile } from '@/service/profile-service';
 import LoginForm from './forms/loginForm';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog"
+import { useEffect, useState } from "react";
 
 export default function Login() {
     const router = useRouter()
     const userProfile = useUserProfile()
+    const [openDialog, setOpenDialog] = useState(true)
 
     if (userProfile.data) {
         router.push("/ticklist")
@@ -16,7 +25,6 @@ export default function Login() {
     const signinMutation = useSigninMutation()
 
     const handleSubmit = ({ email, password }: { email: string, password: string }) => {
-
         toast.promise(signinMutation.mutateAsync({ email, password }), {
             loading: "Logging in...",
             success: "Login successful.",
@@ -24,7 +32,27 @@ export default function Login() {
         }).then(() => router.push("/ticklist"))
 
     }
+    useEffect(() => {
+        const isFirstVisit = localStorage.getItem("isFirstVisit")
+        if (!isFirstVisit) {
+            return setOpenDialog(true)
+        }
+        localStorage.setItem("isFirstVisit", "true")
 
-    return <LoginForm onSubmit={handleSubmit} />
+    }, [])
+
+    return <>
+        <Dialog open={openDialog} onOpenChange={setOpenDialog} modal={true}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle className="mb-4 text-xl">Hola Amigo!</DialogTitle>
+                    <DialogDescription className="text-lg">
+                        This app is running on free tier. Please allow backend service to start it may take 1-2 minutes.
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+        <LoginForm onSubmit={handleSubmit} />
+    </>
 
 }
